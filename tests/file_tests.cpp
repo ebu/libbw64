6a665d6964aebebe18ea1cba706c87e0892cd992
Benjamin Weiss <benjamin.weiss@irt.de>
2018-07-31 11:21:35 +0200

    move test_files to `tests` subfolder and renamed it + renamed catch main test file

--- tests/file_tests.cpp
+++ tests/file_tests.cpp
@@ -5,7 +5,7 @@
 using namespace bw64;
 
 TEST_CASE("rect_16bit") {
-  Bw64Reader bw64File("testfiles/rect_16bit.wav");
+  Bw64Reader bw64File("rect_16bit.wav");
   REQUIRE(bw64File.bitDepth() == 16);
   REQUIRE(bw64File.sampleRate() == 44100);
   REQUIRE(bw64File.channels() == 2);
@@ -13,7 +13,7 @@ TEST_CASE("rect_16bit") {
 }
 
 TEST_CASE("rect_24bit") {
-  Bw64Reader bw64File("testfiles/rect_24bit.wav");
+  Bw64Reader bw64File("rect_24bit.wav");
   REQUIRE(bw64File.bitDepth() == 24);
   REQUIRE(bw64File.sampleRate() == 44100);
   REQUIRE(bw64File.channels() == 2);
@@ -21,7 +21,7 @@ TEST_CASE("rect_24bit") {
 }
 
 TEST_CASE("rect_32bit") {
-  Bw64Reader bw64File("testfiles/rect_32bit.wav");
+  Bw64Reader bw64File("rect_32bit.wav");
   REQUIRE(bw64File.bitDepth() == 32);
   REQUIRE(bw64File.sampleRate() == 44100);
   REQUIRE(bw64File.channels() == 2);
@@ -29,7 +29,7 @@ TEST_CASE("rect_32bit") {
 }
 
 TEST_CASE("rect_24bit_rf64") {
-  Bw64Reader bw64File("testfiles/rect_24bit_rf64.wav");
+  Bw64Reader bw64File("rect_24bit_rf64.wav");
   REQUIRE(bw64File.bitDepth() == 24);
   REQUIRE(bw64File.sampleRate() == 44100);
   REQUIRE(bw64File.channels() == 2);
@@ -37,22 +37,20 @@ TEST_CASE("rect_24bit_rf64") {
 }
 
 TEST_CASE("rect_24bit_noriff") {
-  REQUIRE_THROWS_AS(Bw64Reader("testfiles/rect_24bit_noriff.wav"),
-                    std::runtime_error);
+  REQUIRE_THROWS_AS(Bw64Reader("rect_24bit_noriff.wav"), std::runtime_error);
 }
 
 TEST_CASE("rect_24bit_nowave") {
-  REQUIRE_THROWS_AS(Bw64Reader("testfiles/rect_24bit_nowave.wav"),
-                    std::runtime_error);
+  REQUIRE_THROWS_AS(Bw64Reader("rect_24bit_nowave.wav"), std::runtime_error);
 }
 
 TEST_CASE("rect_24bit_wrong_fmt_size") {
-  REQUIRE_THROWS_AS(Bw64Reader("testfiles/rect_24bit_wrong_fmt_size.wav"),
+  REQUIRE_THROWS_AS(Bw64Reader("rect_24bit_wrong_fmt_size.wav"),
                     std::runtime_error);
 }
 
 TEST_CASE("noise_24bit_uneven_data_chunk_size") {
-  Bw64Reader bw64File("testfiles/noise_24bit_uneven_data_chunk_size.wav");
+  Bw64Reader bw64File("noise_24bit_uneven_data_chunk_size.wav");
   REQUIRE(bw64File.bitDepth() == 24);
   REQUIRE(bw64File.sampleRate() == 44100);
   REQUIRE(bw64File.channels() == 1);
