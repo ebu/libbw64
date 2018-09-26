f09536abea58a5beb6aac2e6d2502812876eba75
Benjamin Weiss <benjamin.weiss@irt.de>
2018-09-26 18:59:28 +0200

    added write and write/read unit tests

--- tests/file_tests.cpp
+++ tests/file_tests.cpp
@@ -58,6 +58,101 @@ TEST_CASE("noise_24bit_uneven_data_chunk_size") {
   REQUIRE(bw64File.chnaChunk() != nullptr);  // XXX
 }
 
+TEST_CASE("write_16bit") {
+  auto bw64File = writeFile("zeros_16bit.wav", 2u, 48000u, 16u);
+
+  REQUIRE(bw64File->channels() == 2);
+  REQUIRE(bw64File->sampleRate() == 48000);
+  REQUIRE(bw64File->bitDepth() == 16);
+
+  int frames = 4800;
+  std::vector<float> data(frames * bw64File->channels(), 0.0);
+  bw64File->write(&data[0], frames);
+  REQUIRE(bw64File->framesWritten() == frames);
+}
+
+TEST_CASE("write_24bit") {
+  auto bw64File = writeFile("zeros_24bit.wav", 2u, 48000u, 24u);
+
+  REQUIRE(bw64File->channels() == 2);
+  REQUIRE(bw64File->sampleRate() == 48000);
+  REQUIRE(bw64File->bitDepth() == 24);
+
+  int frames = 4800;
+  std::vector<float> data(frames * bw64File->channels(), 0.0);
+  bw64File->write(&data[0], frames);
+  REQUIRE(bw64File->framesWritten() == frames);
+}
+
+TEST_CASE("write_32bit") {
+  auto bw64File = writeFile("zeros_32bit.wav", 2u, 48000u, 32u);
+
+  REQUIRE(bw64File->channels() == 2);
+  REQUIRE(bw64File->sampleRate() == 48000);
+  REQUIRE(bw64File->bitDepth() == 32);
+
+  int frames = 4800;
+  std::vector<float> data(frames * bw64File->channels(), 0.0);
+  bw64File->write(&data[0], frames);
+  REQUIRE(bw64File->framesWritten() == frames);
+}
+
+void writeClipped(const std::string& filename, uint16_t bitDepth,
+                  uint64_t frames, uint16_t channels = 1u,
+                  uint16_t sampleRate = 48000u) {
+  auto bw64File = writeFile(filename, channels, sampleRate, bitDepth);
+  std::vector<float> data(frames * channels, 2.f);
+  bw64File->write(&data[0], frames);
+}
+
+TEST_CASE("write_read_16bit_clipped") {
+  int frames = 4800;
+  writeClipped("write_read_16bit_clipped.wav", 16, frames);
+  auto bw64File = readFile("write_read_16bit_clipped.wav");
+  std::vector<float> data(frames * bw64File->channels(), 0.f);
+  auto readFrames = bw64File->read(&data[0], frames);
+  REQUIRE(readFrames == frames);
+  REQUIRE(data.at(0) == Approx(1.f).epsilon(1e-2));
+  REQUIRE(data.at(100) == Approx(1.f).epsilon(1e-2));
+  REQUIRE(data.at(200) == Approx(1.f).epsilon(1e-2));
+  REQUIRE(data.at(400) == Approx(1.f).epsilon(1e-2));
+  REQUIRE(data.at(800) == Approx(1.f).epsilon(1e-2));
+  REQUIRE(data.at(1600) == Approx(1.f).epsilon(1e-2));
+  REQUIRE(data.at(3200) == Approx(1.f).epsilon(1e-2));
+}
+
+TEST_CASE("write_read_24bit_clipped") {
+  int frames = 4800;
+  writeClipped("write_read_24bit_clipped.wav", 24, frames);
+  auto bw64File = readFile("write_read_24bit_clipped.wav");
+  std::vector<float> data(frames * bw64File->channels(), 0.f);
+  auto readFrames = bw64File->read(&data[0], frames);
+  REQUIRE(readFrames == frames);
+  REQUIRE(data.at(0) == Approx(1.f).epsilon(1e-3));
+  REQUIRE(data.at(100) == Approx(1.f).epsilon(1e-3));
+  REQUIRE(data.at(200) == Approx(1.f).epsilon(1e-3));
+  REQUIRE(data.at(400) == Approx(1.f).epsilon(1e-3));
+  REQUIRE(data.at(800) == Approx(1.f).epsilon(1e-3));
+  REQUIRE(data.at(1600) == Approx(1.f).epsilon(1e-3));
+  REQUIRE(data.at(3200) == Approx(1.f).epsilon(1e-3));
+}
+
+TEST_CASE("write_read_32bit_clipped") {
+  int frames = 4800;
+  writeClipped("write_read_32bit_clipped.wav", 32, frames);
+  auto bw64File = readFile("write_read_32bit_clipped.wav");
+  std::vector<float> data(frames * bw64File->channels(), 0.f);
+  auto readFrames = bw64File->read(&data[0], frames);
+  REQUIRE(readFrames == frames);
+  REQUIRE(data.at(0) == Approx(1.f).epsilon(1e-6));
+  REQUIRE(data.at(100) == Approx(1.f).epsilon(1e-6));
+  REQUIRE(data.at(200) == Approx(1.f).epsilon(1e-6));
+  REQUIRE(data.at(400) == Approx(1.f).epsilon(1e-6));
+  REQUIRE(data.at(800) == Approx(1.f).epsilon(1e-6));
+  REQUIRE(data.at(1600) == Approx(1.f).epsilon(1e-6));
+  REQUIRE(data.at(3200) == Approx(1.f).epsilon(1e-6));
+}
+
 TEST_CASE("can_read_all_frames") {
   int frames = 13;
   auto bw64File = readFile("noise_24bit_uneven_data_chunk_size.wav");
