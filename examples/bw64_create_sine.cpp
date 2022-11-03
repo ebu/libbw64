14391322042a742ef49c843c715e1d2c8d79c2df
Thomas Nixon <tom@tomn.co.uk>
2022-11-03 10:28:20 +0000

    don't use non-standard M_PI macro

--- examples/bw64_create_sine.cpp
+++ examples/bw64_create_sine.cpp
@@ -1,4 +1,3 @@
-#define _USE_MATH_DEFINES
 #include <cmath>
 #include <iostream>
 #include <bw64/bw64.hpp>
@@ -7,6 +6,7 @@ const float DURATION = 10.f;  // seconds
 const float FREQUENCY = 440.f;  // Hz
 const float GAIN = 0.5f;  // Hz
 const uint32_t BLOCK_SIZE = 512;
+const float PI = 3.14159265358979323846f;
 
 using namespace bw64;
 
@@ -22,11 +22,11 @@ int main(int argc, char const* argv[]) {
       static_cast<uint32_t>(DURATION * bw64File->sampleRate());
   float sineState = 0.f;
   float normF =
-      static_cast<float>(2.f * M_PI * FREQUENCY / bw64File->sampleRate());
+      static_cast<float>(2.f * PI * FREQUENCY / bw64File->sampleRate());
   while (bw64File->framesWritten() < frameDuration) {
     std::vector<float> buffer;
     for (uint32_t i = 0; i < BLOCK_SIZE; ++i) {
-      sineState = static_cast<float>(std::fmod(sineState + normF, 2.f * M_PI));
+      sineState = static_cast<float>(std::fmod(sineState + normF, 2.f * PI));
       auto value = static_cast<float>(std::sin(sineState) * GAIN);
       buffer.push_back(value);
     }
